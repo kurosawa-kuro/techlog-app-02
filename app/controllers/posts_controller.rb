c71e5635408e4646542a2382472a11516c565ae6
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-10-03 12:27:30 +0000

    init

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id # ログインユーザのIDを代入
+    if @post.save
+      flash[:notice] = '投稿しました'
+      redirect_to root_path
+    else
+      flash[:alert] = '投稿に失敗しました'
+      render :new
+    end
+  end
+
+  def index # 追加
+    @posts = Post.limit(10).order(created_at: :desc)
+  end
+
+  def show # 追加
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    if @post.user == current_user
+      @post.destroy
+      flash[:notice] = '投稿が削除されました'
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'posts#index'
+
+  resources :posts, only: [ :index, :new, :create, :show, :destroy]
+end
